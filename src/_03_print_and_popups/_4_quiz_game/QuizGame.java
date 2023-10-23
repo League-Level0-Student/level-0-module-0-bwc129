5efc146a5707c9a8db86734530783a9596a30a9e
league <league@imac-12.attlocal.net>
2023-10-23 16:58:35 -0700

    d

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -6,20 +6,37 @@ public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score= 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-		String question = JOptionPane.showInputDialog("What is 1+1?");
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
+		String gh = JOptionPane.showInputDialog("What is 1+1?");
 		
+				// 3.  Use an if statement to check if their answer is correct
+		if (gh.equals ("2")) {
+			
+				// 4.  if the user's answer was correct, add one to their score 
+	score++;}
+		else {
+			score--;}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		String hiff = JOptionPane.showInputDialog("What is 2+2?");
+		if (hiff.equals ("4")) {
+			score++;}
+		else {
+			score--;}
+		String sd = JOptionPane.showInputDialog("What is 5+2?");
+		if (sd.equals ("7")) {
+			score++;}
+		else {
+			score--;}
 		// After all the questions have been asked, tell the user their final score 
-		
+	
+	JOptionPane.showMessageDialog(null,score);
 	}
-}
+	
+	
+	
+	}
+
