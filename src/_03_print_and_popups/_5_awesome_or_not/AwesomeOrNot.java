4247c0842d61d01e77dd494ff58a81a661a1f0b0
league <league@imac-12.attlocal.net>
2023-10-30 16:58:54 -0700

    skibiti

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,46 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
+public static void main(String[] args) {
+	
 
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+		int num= 0;
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+		num =ran.nextInt(4); 
 		// 3. Print your variable to the console
-	
+			System.out.println();																																																																																																																																																																																																																																																																															
 		// 4. Get the user to enter something that they think is awesome
-	
+			String gh = JOptionPane.showInputDialog("What is awesome?");
 		// 5. If your variable is  0
-	
+	 num =0;
 			// -- tell the user whatever they entered is awesome!
-	
+	 JOptionPane.showMessageDialog(null,"GOOFY");
 		// 6. If your variable is  1
-	
-			// -- tell the user whatever they entered is ok.
+	 num =1;
+		
+JOptionPane.showMessageDialog(null,"COOL");
+			// -- tell the user whatever they entered is okay.
 	
 		// 7. If your variable is  2
-	
+num =2;
+
+JOptionPane.showMessageDialog(null,"BORING");
 			// -- tell the user whatever they entered is boring.
 	
 		// 8. If your variable is  3
-	
+num =3;
+
+JOptionPane.showMessageDialog(null,"HI");
 			// -- invent your own message to give to the user (be nice).
 
+}
 }
--- src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
+++ src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
@@ -27,8 +27,8 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	
 	boolean started = false;
 	BufferedImage maze;
-	final int frameWidth = 500;
-	final int frameHeight = 500;
+	final int frameWidth = 1000;
+	final int frameHeight = 1000;
 
 	ScaryMaze() throws Exception {
 		
@@ -39,7 +39,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		
 		
 		//2. Change the line of code below so that it uses YOUR maze's file name
-		maze = ImageIO.read(getClass().getResource("standardMaze.png"));
+		maze = ImageIO.read(getClass().getResource("MAZE.png"));
 		
 		
 		//3. Run the program. Do you see your maze? Don't continue until you do.
@@ -57,7 +57,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseColor = maze.getRGB(mouseX, mouseY);	
 		
 		//4. Print the mouseColor variable (Hint: use syso)
-		
+		System.out.println(mouseColor);
 		//5.  Run your program and move your mouse over the START COLOR. A number will be printed to the console
 		
 		
